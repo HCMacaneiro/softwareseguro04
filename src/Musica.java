18b80e512242278a77bee6678e157e03877f33df
Henrique Cardoso Macaneiro <hcmacaneiro@gmail.com>
2024-03-13 21:11:33 -0300

    Arruma classe Musica

--- src/Musica.java
+++ src/Musica.java
@@ -1,28 +1,11 @@
 public class Musica {
     private String titulo;
-    private String duracao;
-    private String compositor;
 
-    public Musica(String titulo, String duracao, String compositor) {
+    public Musica(String titulo) {
         this.titulo = titulo;
-        this.duracao = duracao;
-        this.compositor = compositor;
-    }
-
-    public void TocarMusica() {
-        System.out.println("Tocando " + this.titulo + " de " + this.compositor + " com duração " + this.duracao);
-    }
-
-
-    public String getCompositor() {
-        return compositor;
     }
 
     public String getTitulo() {
         return titulo;
-    }
-
-    public String getDuracao() {
-        return duracao;
     }
 }

16034e969b46133f4c4a0dc3bc971a23989c0f92
Henrique Cardoso Macaneiro <hcmacaneiro@gmail.com>
2024-03-13 21:14:53 -0300

    Cria album e adiciona 5 musicas

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        Album AlbumPagode = new Album("Pagodinho da massa", 2002, "Pagodin");
+        Musica musica1 = new Musica("Pagode do mal");
+        Musica musica2 = new Musica("Pagode do bem");
+        Musica musica3 = new Musica("Pagode da massa");
+        Musica musica4 = new Musica("Pagode dos loucos");
+        Musica musica5 = new Musica("Pagode do pagode");
+        AlbumPagode.addMusicas(musica1);
+        AlbumPagode.addMusicas(musica2);
+        AlbumPagode.addMusicas(musica3);
+        AlbumPagode.addMusicas(musica4);
+        AlbumPagode.addMusicas(musica5);
     }
 }
 

d72a7f1ff11f8b0cacdb77514c982902c4d2d552
Henrique Cardoso Macaneiro <hcmacaneiro@gmail.com>
2024-03-13 21:05:02 -0300

    Muda arraylist de músicas

--- src/Album.java
+++ src/Album.java
@@ -3,15 +3,15 @@ import java.util.ArrayList;
 public class Album {
     private int ano;
     private String nome;
-    private ArrayList<String> musicas;
+    private ArrayList<Musica> musicas;
 
     public Album(String genero, int ano, String nome, String artista) {
         this.ano = ano;
         this.nome = nome;
-        this.musicas = new ArrayList<String>();
+        this.musicas = new ArrayList<Musica>();
     }
 
-    public void setMusicas(ArrayList<String> musicas) {
+    public void setMusicas(ArrayList<Musica> musicas) {
         this.musicas = musicas;
     }
 }
